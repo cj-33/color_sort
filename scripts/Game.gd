538d1c1e8b159d3b3e5fc4cd4f74320a4f917053
Jeremy Armstrong <4165806+cj-33@users.noreply.github.com>
2026-06-28 15:28:56 -0600

    +game scene

---
+++ scripts/Game.gd
@@ -0,0 +1,7 @@
+# Main controller for the game scene
+class_name GameManager
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
--- scripts/StartScreen.gd
+++ scripts/StartScreen.gd
@@ -4,6 +4,4 @@ func _ready() -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
-	# Change to the game scene when it exists
-	# get_tree().change_scene_to_file("res://game.tscn")
-	print("Start button pressed! (Game scene not created yet)")
+	get_tree().change_scene_to_file("res://game.tscn")
