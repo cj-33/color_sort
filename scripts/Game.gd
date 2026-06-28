097a0f0d300d9af086f7568a7088128b2387f56d
Jeremy Armstrong <4165806+cj-33@users.noreply.github.com>
2026-06-28 15:51:41 -0600

    phone back button goes from game screen back to start screen

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -4,4 +4,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+    pass
+
+# Handle system events like the Android back button
+func _notification(what: int) -> void:
+    if what == NOTIFICATION_WM_GO_BACK_REQUEST:
+        get_tree().change_scene_to_file("res://start.tscn")
